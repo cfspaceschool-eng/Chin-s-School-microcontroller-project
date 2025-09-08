f9e50b80c6297c68f492e7f9abc6b2fd7b232183
CF-Space-at-school-pc <cfspace.school@gmail.com>
2025-09-08 17:16:21 +0700

    Add files via upload

---
+++ sketch_sep8a/sketch_sep8a.ino
@@ -0,0 +1,17 @@
+//no need for a stupid coment LOL buy my dumbass friend still coppy every single line of teacher's code bro think it youself bro🤣
+#include<KG.h> //wait wait wait how the fuck can i get this libary i should ask my teacher for his own libary lol😂
+void setup() 
+{
+GO();//button switch>D@ >GND
+}
+
+void loop() 
+{
+FLR(100,); //wait bro what the fuck is this command actually i should go ask my teacher for it😂 ok teacher tell me it's speed pass L / R
+delay(2000);
+STOP(); delay(5000);
+TL(100); delay(2000); STOP(); delay(5000);
+TR(100); delay(2000); STOP(); delay(5000);
+BLR(100,100); delay(2000); STOP(); delay(5000);
+}
+//wow so impressive my teacher he make his own libary wow wow actually i never think he could do something like this bc lasttime i saw he use ubuntu gnome as server i tought he don't know anythinglol
