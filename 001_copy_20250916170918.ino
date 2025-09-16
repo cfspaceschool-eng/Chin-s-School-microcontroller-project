c6302e5aa2434ce73c43a6e4103c27cf28ca35b1
CF-Space-at-school-pc <cfspace.school@gmail.com>
2025-09-16 17:12:29 +0700

    16/9/2025

---
+++ 001_copy_20250916170918.ino
@@ -0,0 +1,42 @@
+#include<KG2.h>
+void setup()
+{
+OK();
+ OLED.clearDisplay();
+
+ OLED.setCursor(0,0);
+ OLED.prine("HELLO");
+ OLED.print("SPARTAN");
+
+
+ OLED.display();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
