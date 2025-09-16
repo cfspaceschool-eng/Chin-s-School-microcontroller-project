7a4bcea82428e71e7a987b835379da82e28ee128
CF-Space-at-school-pc <cfspace.school@gmail.com>
2025-09-16 17:42:19 +0700

    Done

--- 001_copy_20250916170918.ino
+++ 001_copy_20250916170918.ino
@@ -3,40 +3,50 @@ void setup()
 {
 OK();
  OLED.clearDisplay();
-
  OLED.setCursor(0,0);
- OLED.prine("HELLO");
- OLED.print("SPARTAN");
 
+}
+void loop()
+{
 
+ OLED.print("10");
+ delay(1000);
+ OLED.display();
+  delay(1000);
+ OLED.clearDisplay();
+ OLED.print("9");
+  delay(1000);
+ OLED.display();
+  OLED.clearDisplay();
+ OLED.print("8");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("7");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("6");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("5");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("4");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("3");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("2");
+ OLED.display();
+  OLED.clearDisplay();
+   delay(1000);
+ OLED.print("1");
  OLED.display();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
